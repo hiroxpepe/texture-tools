995abbe8386a5b68bd83984d705256f6cbf79498
Hiroyuki Adachi <hiroxpepe@gmail.com>
2023-04-23 22:39:23 +0900

    update

--- Texture.Draw/Facade.cs
+++ Texture.Draw/Facade.cs
@@ -40,10 +40,10 @@ namespace Texture {
         // public Methods [verb, verb phrases]
 
         public static void Execute() {
-            Cut cut1 = NewCutByPiece(piece_count_x: 7, piece_count_y: 7);
-            Face face1 = NewFace(width: 256, hight: 256, cut: cut1);
+            Cut cut1 = NewCutByPiece(piece_count_x: 8, piece_count_y: 8);
+            Face face1 = NewFace(width: 512, hight: 512, cut: cut1);
             Cut cut2 = NewCutByPiece(piece_count_x: 5, piece_count_y: 5);
-            Face face2 = NewFace(width: 256, hight: 256, cut: cut2);
+            Face face2 = NewFace(width: 512, hight: 512, cut: cut2);
             using Tool tool = NewTool(face_array: new Face[] { face1, face2 });
             // layer 1.
             int count = 0; int img_idx = 0;
@@ -51,10 +51,10 @@ namespace Texture {
                 // randomize
                 face1.AllPoint.ForEach(action: x => {
                     x.Where(predicate: x => !x.FixedX).ToList().ForEach(action: x => {
-                        if (!x.MovedX) { x.X += _random.Next(minValue: -10, maxValue: 10); x.MovedX = true; }
+                        if (!x.MovedX) { x.X += _random.Next(minValue: -12, maxValue: 12); x.MovedX = true; }
                     });
                     x.Where(predicate: x => !x.FixedY).ToList().ForEach(action: x => {
-                        if (!x.MovedY) { x.Y += _random.Next(minValue: -10, maxValue: 10); x.MovedY = true; }
+                        if (!x.MovedY) { x.Y += _random.Next(minValue: -12, maxValue: 12); x.MovedY = true; }
                     });
                 });
                 // fills by color.
@@ -62,6 +62,10 @@ namespace Texture {
                 face1.AllPoint.ForEach(action: x => {
                     Color color = NextIndex() % 2 == 0 ? Color.Red : Color.Yellow;
                     tool.Fill(points: x, color: color, img_idx: img_idx, cell_idx: count, debug: false);
+                    int random_number = _random.Next(1, (face1.AllPoint.Count / 2) + 1);
+                    if (random_number == 1) {
+                        tool.Fill(points: x, color: Color.Magenta, img_idx: img_idx, cell_idx: count, debug: false);
+                    }
                     count++;
                 });
             };
@@ -91,9 +95,9 @@ namespace Texture {
                 });
             };
             face2.OnWrite += () => {
-                tool.Write(img_idx);
+                //tool.Write(img_idx);
             };
-            face2.Make();
+            //face2.Make();
         }
     }
 }
